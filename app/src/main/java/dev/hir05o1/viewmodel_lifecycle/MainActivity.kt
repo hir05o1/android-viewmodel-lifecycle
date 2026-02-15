ed1f6bb45eb5bedda778e8744ca45940579fc966
hir05o1 <100586810+hir05o1@users.noreply.github.com>
2026-02-15 22:51:35 +0900

    add: Implement lifecycle logging in MainActivity

--- app/src/main/java/dev/hir05o1/viewmodel_lifecycle/MainActivity.kt
+++ app/src/main/java/dev/hir05o1/viewmodel_lifecycle/MainActivity.kt
@@ -1,6 +1,7 @@
 package dev.hir05o1.viewmodel_lifecycle
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,6 +10,7 @@ import dev.hir05o1.viewmodel_lifecycle.ui.theme.ViewModelLifecycleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("Lifecycle", "MainActivity - onCreate")
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
@@ -16,5 +18,35 @@ class MainActivity : ComponentActivity() {
                 CounterView()
             }
         }
+    }
+
+    override fun onStart() {
+        Log.i("Lifecycle", "MainActivity - onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.i("Lifecycle", "MainActivity - onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.i("Lifecycle", "MainActivity - onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.i("Lifecycle", "MainActivity - onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.i("Lifecycle", "MainActivity - onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        Log.i("Lifecycle", "MainActivity - onRestart")
+        super.onRestart()
     }
 }
